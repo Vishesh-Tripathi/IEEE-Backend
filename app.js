dc6120ed025a827671be9771daf694db30226794
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-10-04 14:24:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ import eventrouter from './Routes/events.routes.js'
 import trouter from './Routes/team.routes.js';
 import hrouter from './Routes/Highlights.routes.js';
 
-const app = express();
+app.use(cors({
+    origin: 'https://ieee-frontend-j0losar5y-vishesh-tripathis-projects.vercel.app/',
+    credentials: "include"
+}))
 app.use(cors());
 app.use(express.json({limit :"16kb"}))
 app.use(express.urlencoded({extended :true ,limit:"16kb"}))
