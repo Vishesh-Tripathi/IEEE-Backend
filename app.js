0ab9e338f9f05579d8dd14f1d07a7dd1ab98c084
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-10-04 14:57:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: ['https://ieee-frontend-j0losar5y-vishesh-tripathis-projects.vercel.app'],
     credentials: "include"
 }))
-app.use(cors());
+// app.use(cors());
 app.use(express.json({limit :"16kb"}))
 app.use(express.urlencoded({extended :true ,limit:"16kb"}))
 app.use(express.static("public"))
