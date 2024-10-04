2eef97bc045989e47be6cd5511975f9296990a43
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-10-04 15:37:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ import eventrouter from './Routes/events.routes.js'
 import trouter from './Routes/team.routes.js';
 import hrouter from './Routes/Highlights.routes.js';
 const app = express();
+
+
 app.use(cors({
-    origin: ['https://ieee-frontend-two.vercel.app'],
-    credentials: "include"
-}))
+    origin: ['https://ieee-frontend-two.vercel.app'],  // Explicitly set the allowed origin
+    credentials: true  // This allows credentials (cookies, authentication) to be sent
+}));
+
 // app.use(cors());
 app.use(express.json({limit :"16kb"}))
 app.use(express.urlencoded({extended :true ,limit:"16kb"}))
